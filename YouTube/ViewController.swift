b2262036b40c85518870683122f674913ab271d4
Ravil Vildanov <ravilvildanov@air-ravil.dlink>
2018-07-10 14:28:16 +0300

    Initial Commit

---
+++ YouTube/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTube
+//
+//  Created by Равиль Вильданов on 10.07.2018.
+//  Copyright © 2018 Равиль Вильданов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
